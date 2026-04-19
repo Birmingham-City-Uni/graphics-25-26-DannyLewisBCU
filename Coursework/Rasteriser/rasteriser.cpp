9e8ae53d845115b1c736606d499ed8ccd33141f4
Danny Lewis <danny.lewis@mail.bcu.ac.uk>
2026-04-19 12:52:16 +0100

    rendered a cube using triangles

--- Coursework/Rasteriser/rasteriser.cpp
+++ Coursework/Rasteriser/rasteriser.cpp
@@ -40,15 +40,6 @@ int main()
 	std::vector<uint8_t> imageBuffer(height*width*nChannels);
 	std::vector<float> depthBuffer(width * height, std::numeric_limits<float>::infinity());
 
-	Vec3 v0 = {-1.0f, -1.0f, 2.0f };
-	Vec3 v1 = { 1.0f, -1.0f, 2.0f };
-	Vec3 v2 = { 0.0f, 1.0f, 3.0f };
-
-	Vec2 p0 = project(v0, width, height);
-	Vec2 p1 = project(v1, width, height);
-	Vec2 p2 = project(v2, width, height);
- 
-
 	// Cyan background
     for(int y = 0; y < height; ++y) 
 		for (int x = 0; x < width; ++x) {
@@ -59,48 +50,72 @@ int main()
 			imageBuffer[pixelIdx * nChannels + 3] = 255; 
 		}
 
-	int minX = std::min({ (int)p0.x, (int)p1.x, (int)p2.x });
-	int maxX = std::max({ (int)p0.x, (int)p1.x, (int)p2.x });
-	int minY = std::min({ (int)p0.y, (int)p1.y, (int)p2.y });
-	int maxY = std::max({ (int)p0.y, (int)p1.y, (int)p2.y });
+	std::vector<Vec3> vertices = {
+		{ -1, -1, 3}, {1, -1, 3}, {1, 1, 3}, {-1, 1, 3},
+		{  -1, -1, 5}, {1, -1, 5}, {1, 1, 5}, {-1, 1, 5}
+	};
 
-	minX = std::max(minX, 0);
-	maxX = std::min(maxX, width - 1);
-	minY = std::max(minY, 0);
-	maxY = std::min(maxY, height - 1);
+	std::vector<std::vector<int>> triangles = {
+		{0, 1, 2}, {0, 2, 3},
+		{4, 5, 6}, {4, 6, 7},
+		{0, 1, 5}, {0, 5, 4},
+		{2, 3, 7}, {2, 7, 6},
+		{1, 2, 6}, {1, 6, 5},
+		{0, 3, 7}, {0, 7, 4}
 
-	float area = edgeFunction(p0, p1, p2);
+	};
 
-    for (int y = 0; y < height; ++y) {
-		for (int x = 0; x < width; ++x) {
-			Vec2 p = {x + 0.5f, y + 0.5f};
-			float w0 = edgeFunction(p1, p2, p);
-			float w1 = edgeFunction(p2, p0, p);
-			float w2 = edgeFunction(p0, p1, p);
-			
-			if (
-				(w0 >= 0 && w1 >= 0 && w2 >= 0 && area > 0) ||
-				(w0 <= 0 && w1 <= 0 && w2 <= 0 && area < 0)
-			) {
+	for (auto& tri : triangles) 
+	{
 
-				w0 /= area;
-				w1 /= area;
-				w2 /= area;
+		Vec3 v0 = vertices[tri[0]];
+		Vec3 v1 = vertices[tri[1]];
+		Vec3 v2 = vertices[tri[2]];
 
-				float depth = w0 * v0.z + w1 * v1.z + w2 * v2.z;
-				int pixelIdx = x + y * width;
+		Vec2 p0 = project(v0, width, height);
+		Vec2 p1 = project(v1, width, height);
+		Vec2 p2 = project(v2, width, height);
 
-				if (depth < depthBuffer[pixelIdx]) {
-					depthBuffer[pixelIdx] = depth;
+		int minX = std::max(0, (int)std::min({ p0.x, p1.x, p2.x }));
+		int maxX = std::min(width - 1, (int)std::max({ p0.x, p1.x, p2.x }));
+		int minY = std::max(0, (int)std::min({ p0.y, p1.y, p2.y }));
+		int maxY = std::min(height - 1, (int)std::max({ p0.y, p1.y, p2.y }));
+
+		float area = edgeFunction(p0, p1, p2);
+
+		for (int y = minY; y <= maxY; ++y)
+			for (int x = minX; x <= maxX; ++x) {
+
+				Vec2 p = { x + 0.5f, y + 0.5f };
+
+				float w0 = edgeFunction(p1, p2, p);
+				float w1 = edgeFunction(p2, p0, p);
+				float w2 = edgeFunction(p0, p1, p);
+
+				if (
+					(w0 >= 0 && w1 >= 0 && w2 >= 0 && area > 0) ||
+					(w0 <= 0 && w1 <= 0 && w2 <= 0 && area < 0)
+					){
+					w0 /= area;
+					w1 /= area;
+					w2 /= area;
+
+					float depth = w0 * v0.z + w1 * v1.z + w2 * v2.z;
+
+					int pixelIdx = x + y * width;
+
+					if (depth < depthBuffer[pixelIdx]) {
+						depthBuffer[pixelIdx] = depth;
+						imageBuffer[pixelIdx * nChannels + 0] = 255; 
+						imageBuffer[pixelIdx * nChannels + 1] = 0;
+						imageBuffer[pixelIdx * nChannels + 2] = 0; 
+						imageBuffer[pixelIdx * nChannels + 3] = 255; 
+					}
 				}
-
-				imageBuffer[pixelIdx * nChannels + 0] = 255; 
-				imageBuffer[pixelIdx * nChannels + 1] = 0; 
-				imageBuffer[pixelIdx * nChannels + 2] = 0; 
-				imageBuffer[pixelIdx * nChannels + 3] = 255; 
 			}
-		}
 	}
+
+
 
     // Save the image
     int errorCode;
